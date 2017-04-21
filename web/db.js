3e087213142cf423083e08d773b8395b521f4ae6
Brandon Denton <bdenton418@gmail.com>
2017-04-21 17:54:32 -0400

    general structure of db, will need to tweak more

--- web/db.js
+++ web/db.js
@@ -12,3 +12,35 @@ const knex = require('knex')({
 });
 
 module.exports = knex; 
+
+//building the database
+//articles
+knex.schema.createTableIfNotExists('articles', function (table) {
+    table.increments();
+    table.string('articleID');
+    table.string('name');
+    table.string('userID');     // author
+    table.int('userVotes');
+})
+
+//users
+knex.schema.createTableIfNotExists('users', function (table) {
+    table.increments();
+    table.string('userID');
+    // will return to this later for other items
+})
+
+//roles
+knex.schema.createTableIfNotExists('roles', function (table) {
+    table.increments();
+    table.string('roleID');
+    table.string('userID');
+}
+
+//tags/categories
+knex.schema.createTableIfNotExists('tags', function (table) {
+    table.increments();
+    table.string('categoryName');
+    table.string('articleID');
+    // will come back to this later for other items
+})
