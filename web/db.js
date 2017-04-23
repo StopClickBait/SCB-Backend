c73d51abf86c4cd8baec161526f308141ef099ae
Brandon Denton <bdenton418@gmail.com>
2017-04-22 20:33:38 -0400

    looking through repo to see how to link

--- web/db.js
+++ web/db.js
@@ -1,118 +1,110 @@
-//'use strict';
+//namespace SCB?
 
-/*const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER  ,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE,
-    ssl: true
-  },
-});
+use Illuminate\Database\Eloquent\Model;
 
-module.exports = knex; */
+class SCB extends Model 
+{
+    //building the database
+    //articles
+    if (!(Schema::hasTable('articles'))) {    
+        Schema::create('articles', function($table) {
+            $table->increments('id')->unsigned();
+            $table->string('nameURI');
+            $table->boolean('isDeleted');
+            $table->foreign('userID')
+                ->references('id')
+                ->on('users');
+        //table->integer->unsigned('tagID')
+        //table->integer->unsigned('userVotes');
+        })
+    }
 
-//building the database
-//articles
-if (!(Schema::hasTable('articles'))) {    
-    Schema::create('articles', function($table) {
-        $table->increments('id')->unsigned();
-        $table->string('nameURI');
-        $table->boolean('isDeleted');
-        $table->foreign('userID')
-            ->references('id')
-            ->on('users');
-    //table->integer->unsigned('tagID')
-    //table->integer->unsigned('userVotes');
-    })
-}
+    //User Articles
+    if (!(Schema::hasTable('UserArticles'))) {
+        Schema::create('UserArticles', function($table) {
+            $table->increments('id')->unsigned();
+            $table->foreign('articleID')
+                ->references('id')
+                ->on('articles');
+            $table->foreign('userID')
+                ->references('id')
+                ->on('users');
+        })
+    }
+ 
+    //users
+    if (!(Schema::hasTable('users'))) {}
+        Schema::create('users', function($table) {
+            $table->increments('id')->unsigned();
+            $table->string('nameURI');
+            $table->string('authType');   // not sure yet if this will relate to perms or what
+            $table->dateTime('createdDate');
+            $table->dateTime('modifiedDate');
+            $table->boolean('isActive');
+        })
+    }
 
-//User Articles
-if (!(Schema::hasTable('UserArticles'))) {
-    Schema::create('UserArticles', function($table) {
-        $table->increments('id')->unsigned();
-        $table->foreign('articleID')
-            ->references('id')
-            ->on('articles');
-        $table->foreign('userID')
-            ->references('id')
-            ->on('users');
-    })
-}
+    //roles
+    if (!(Schema::hasTable('roles'))) {
+        Schema::create('roles', function($table) {
+            $table->increments('id')->unsigned();
+            $table->string('nameURI');
+            $table->boolean('isDefault');
+            $table->dateTime('createdDate');
+            $table->dateTime('modifiedDate');
+        })
+    }
 
-//users
-if (!(Schema::hasTable('users'))) {}
-    Schema::create('users', function($table) {
-        $table->increments('id')->unsigned();
-        $table->string('nameURI');
-        $table->string('authType');   // not sure yet if this will relate to perms or what
-        $table->dateTime('createdDate');
-        $table->dateTime('modifiedDate');
-        $table->boolean('isActive');
-    })
-}
+    //Role Permissions (relates roles and their permissions)
+    if (!(Schema::hasTable('RolePerms'))) {
+        Schema::create('RolePerms', function($table) {
+            $table->increments('id')->unsigned();
+            $table->foreign('roleID')
+                ->references('id')
+                ->on('roles');
+            $table->foreign('permID')
+                ->references('id')
+                ->on('perms');
+        })
+    }
 
-//roles
-if (!(Schema::hasTable('roles'))) {
-    Schema::create('roles', function($table) {
-        $table->increments('id')->unsigned();
-        $table->string('nameURI');
-        $table->boolean('isDefault');
-        $table->dateTime('createdDate');
-        $table->dateTime('modifiedDate');
-    })
-}
+    //perms
+    if (!(Schema::hasTable('perms'))) {
+        Schema::create('perms', function($table) {
+            $table->increments('id')->unsigned();
+            $table->string('nameURI');
+            $table->dateTime('createdDate');
+            $table->dateTime('modifiedDate');
+        })
+    }
 
-//Role Permissions (relates roles and their permissions)
-if (!(Schema::hasTable('RolePerms'))) {
-    Schema::create('RolePerms', function($table) {
-        $table->increments('id')->unsigned();
-        $table->foreign('roleID')
-            ->references('id')
-            ->on('roles');
-        $table->foreign('permID')
-            ->references('id')
-            ->on('perms');
-    })
-}
+    //tags/categories
+    if (!(Schema::hasTable('tags'))) {
+        Schema::create('tags', function($table) {
+            $table->increments('id')->unsigned();
+            $table->string('categoryName');
+            $table->foreign('articleID')
+                ->references('id')
+                ->on('articles');
+            $table->foreign('userID')
+                ->references('id')
+                ->on('users');
+        })
+    }
 
-//perms
-if (!(Schema::hasTable('perms'))) {
-    Schema::create('perms', function($table) {
-        $table->increments('id')->unsigned();
-        $table->string('nameURI');
-        $table->dateTime('createdDate');
-        $table->dateTime('modifiedDate');
-    })
-}
-
-//tags/categories
-if (!(Schema::hasTable('tags'))) {
-    Schema::create('tags', function($table) {
-        $table->increments('id')->unsigned();
-        $table->string('categoryName');
-        $table->foreign('articleID')
-            ->references('id')
-            ->on('articles');
-        $table->foreign('userID')
-            ->references('id')
-            ->on('users');
-    })
-}
-
-//Article Tags (relation of articles and tags)
-if (!(Schema::hasTable('ArticleTags'))) {
-    Schema::create('ArticleTags', function($table) {
-        $table->increments('id')->unsigned();
-        $table->foreign('tagID')
-            ->references('id')
-            ->on('tags');
-        $table->foreign('articleID')
-            ->references('id')
-            ->on('articles');
-        $table->foreign('userID')
-            ->references('id')
-            ->on('users');          
-    })
+    //Article Tags (relation of articles and tags)
+    if (!(Schema::hasTable('ArticleTags'))) {
+        Schema::create('ArticleTags', function($table) {
+            $table->increments('id')->unsigned();
+            $table->foreign('tagID')
+                ->references('id')
+                ->on('tags');
+            $table->foreign('articleID')
+                ->references('id')
+                ->on('articles');
+            $table->foreign('userID')
+                ->references('id')
+                ->on('users');          
+        })
+    }
 }
