d8f054d63d480214e3a3f83344cdf525b0075b88
cjfravel <cj@cjfravel.com>
2017-05-07 20:32:56 -0400

    Renamed models. Closes #19.

--- backend/app/ArticleTag.php
+++ backend/app/ArticleTag.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Tags extends Model
+class ArticleTag extends Model
 {
     //
 }
--- backend/app/RolePerm.php
+++ backend/app/RolePerm.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ArticleTags extends Model
+class RolePerm extends Model
 {
     //
 }
