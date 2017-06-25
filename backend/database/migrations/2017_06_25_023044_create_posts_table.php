f22891d1e632e0d5587478d0d87236582ed57926
Noah Allen <noahtallen@gmail.com>
2017-06-24 22:39:23 -0400

    Added posts migration

---
+++ backend/database/migrations/2017_06_25_023044_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('userID');
+            $table->integer('articleID');
+            $table->string('text', 140);
+
+            $table->foreign('userID')->references('id')->on('users');
+            $table->foreign('articleID')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
