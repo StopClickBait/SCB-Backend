15b3bde8ac1700c9c0f4838248aa4fd35cd1bf40
Noah Allen <noahtallen@gmail.com>
2017-06-25 13:16:45 -0400

    Add seeds and model factories for the Articles, Users, and Posts tables.

---
+++ backend/database/seeds/PostsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Post::class, 50)->create();
+    }
+}
---
+++ backend/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 50)->create();
+    }
+}
